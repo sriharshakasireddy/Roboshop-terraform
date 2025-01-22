373e0394808e76027aa661ce36fd66c26dcd96fb
iray3 <kasireddy.sriharsha@gmail.com>
2025-01-22 14:57:25 +0530

    dd

--- roboshop/provider.tf
+++ roboshop/provider.tf
@@ -1,6 +1,7 @@
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
-subscription_id = "a92e07d8-3cdd-4fda-bb98-99b2dddb739c"
+
 
 features {}
+  subscription_id = "a92e07d8-3cdd-4fda-bb98-99b2dddb739c"
 }
