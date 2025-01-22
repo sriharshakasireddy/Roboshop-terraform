941b926a64892843f26d0b6431b7a6a0cf3165af
iray3 <kasireddy.sriharsha@gmail.com>
2025-01-22 14:41:27 +0530

    dd

--- roboshop/data.tf
+++ roboshop/data.tf
@@ -4,6 +4,6 @@ data "azurerm_resource_group" "rg" {
 
 data "azurerm_subnet" "subnet" {
   name                 = "default"
-  virtual_network_name = "workshop-vnet "
+  virtual_network_name = "workshop-vnet"
   resource_group_name  = data.azurerm_resource_group.rg.name
 }
