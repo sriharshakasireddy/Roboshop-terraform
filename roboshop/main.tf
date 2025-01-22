2d1c9aaf299e237875a568cfcc8e459dafaec1e7
iray3 <kasireddy.sriharsha@gmail.com>
2025-01-22 14:39:36 +0530

    dd

--- roboshop/main.tf
+++ roboshop/main.tf
@@ -15,7 +15,7 @@ resource "azurerm_virtual_machine" "vm" {
   name                  = "vm1"
   location              = data.azurerm_resource_group.rg.location
   resource_group_name   = data.azurerm_resource_group.rg.name
-  network_interface_ids = azurerm_network_interface.nic.id
+  network_interface_ids = [azurerm_network_interface.nic.id]
   vm_size               = "Standard_DS1_v2"
 
   # Uncomment this line to delete the OS disk automatically when deleting the VM
