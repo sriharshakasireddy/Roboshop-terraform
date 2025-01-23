2babb86bec8e48ac978da5632b641eb1464be672
iray3 <kasireddy.sriharsha@gmail.com>
2025-01-23 22:52:58 +0530

    dd

---
+++ roboshop/module.tf
@@ -0,0 +1,90 @@
+module "components" {
+
+  for_each = var.components
+  source = "./vm"
+  component = each.value["name"]
+  vm_size   = each.value["vm_size"]
+
+}
+
+variable "components" {
+
+  default = {
+
+    frontend = {
+
+      name    = "frontend"
+      vm_size = "Standard_DS1_v2"
+
+    }
+
+    mongodb = {
+
+      name    = "mongodb"
+      vm_size = "Standard_DS1_v2"
+
+    }
+
+    catalogue = {
+
+      name    = "catalogue"
+      vm_size = "Standard_DS1_v2"
+
+    }
+
+    redis = {
+
+      name    = "redis"
+      vm_size = "Standard_DS1_v2"
+
+    }
+
+    user = {
+
+      name    = "user"
+      vm_size = "Standard_DS1_v2"
+
+    }
+
+    cart = {
+
+      name    = "cart"
+      vm_size = "Standard_DS1_v2"
+
+    }
+
+    mysql = {
+
+      name    = "mysql"
+      vm_size = "Standard_DS1_v2"
+
+    }
+
+    shipping = {
+
+      name    = "shipping"
+      vm_size = "Standard_DS1_v2"
+
+    }
+    rabbitmq = {
+
+      name    = "rabbitmq"
+      vm_size = "Standard_DS1_v2"
+
+    }
+
+    payment = {
+
+      name    = "payment"
+      vm_size = "Standard_DS1_v2"
+
+    }
+
+    dispatch = {
+
+      name    = "dispatch"
+      vm_size = "Standard_DS1_v2"
+
+    }
+  }
+}
---
+++ roboshop/vm/main.tf
@@ -0,0 +1,127 @@
+terraform {
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.2"
+    }
+  }
+}
+resource "azurerm_network_interface" "main" {
+  name                = "${var.component}-interface"
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = data.azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.main.id
+  }
+}
+
+
+resource "azurerm_network_security_group" "example" {
+  name                = "${var.component}-nsg"
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+
+  security_rule {
+    name                       = "test123"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    component = var.component
+  }
+}
+resource "azurerm_network_interface_security_group_association" "example" {
+  network_interface_id      = azurerm_network_interface.main.id
+  network_security_group_id = azurerm_network_security_group.example.id
+}
+
+
+resource "azurerm_public_ip" "main" {
+  name                = "${var.component}-pip"
+  resource_group_name = data.azurerm_resource_group.main.name
+  location            = data.azurerm_resource_group.main.location
+  allocation_method   = "Static"
+
+  tags = {
+    component = var.component
+  }
+}
+
+resource "azurerm_dns_a_record" "example" {
+  name                = "${var.component}-dev"
+  zone_name           = "devopsazurepractice.online"
+  resource_group_name = data.azurerm_resource_group.main.name
+  ttl                 = 10
+  records             = [azurerm_network_interface.main.private_ip_address]
+}
+
+resource "azurerm_virtual_machine" "main" {
+  name                  = var.component
+  location              = data.azurerm_resource_group.main.location
+  resource_group_name   = data.azurerm_resource_group.main.name
+  network_interface_ids = [azurerm_network_interface.main.id]
+  vm_size               = "Standard_DS1_v2"
+
+
+
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  delete_os_disk_on_termination = true
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  # delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = var.component
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = var.component
+    admin_username = "harsha"
+    admin_password = "harsha@123456"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags = {
+    component = var.component
+  }
+}
+
+resource "null_resource" "ansible" {
+
+  provisioner "remote-exec" {
+  connection {
+    type     = "ssh"
+    user     = "harsha"
+    password = "harsha@123456"
+    host     = azurerm_public_ip.main.ip_address
+  }
+
+    inline = [
+      "sudo dnf install python3.12-pip -y",
+      "sudo pip3.12 install ansible",
+      "ansible-pull -i localhost, U https://github.com/sriharshakasireddy/ansible roboshop.yml -e app-name=${var.component} -e ENV=dev"
+
+    ]
+  }
+}
